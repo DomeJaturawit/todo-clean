2abb78ccff378eafcd2140a018632b8b31e8afbb
DomeJaturawit <122788011+DomeJaturawit@users.noreply.github.com>
2023-02-10 15:21:34 +0700

    in progress docker

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,33 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"todo-clean/database"
+	"todo-clean/delivery"
+	"todo-clean/repository"
+	"todo-clean/usecase"
+)
+
+func main() {
+
+	dbConn, err := database.ConnectDB()
+	//
+	if err != nil {
+		log.Panicln("failed to connect database", err)
+	} else {
+		fmt.Println("Connect ok", dbConn)
+	}
+	engine := gin.New()
+
+	repo := repository.NewRepository(dbConn)
+	use := usecase.NewUseCase(repo)
+	//
+	delivery.NewHandler(engine, use)
+	//
+	err = engine.Run("localhost:8080")
+	if err != nil {
+		log.Panicln("failed to run", err)
+	}
+}
--- database/db.go
+++ database/db.go
@@ -4,25 +4,47 @@ import (
 	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"os"
-	"todo-clean/common"
+	"log"
+	"todo-clean/repository/model"
 )
 
 func ConnectDB() (client *gorm.DB, err error) {
-	host := os.Getenv(common.PostgresHost)
-	user := os.Getenv(common.PostgresUser)
-	pw := os.Getenv(common.PostgresPassWord)
-	port := os.Getenv(common.PostgresPort)
-	dbName := os.Getenv(common.PostgresDB)
-	ssl := os.Getenv(common.PostgresSSL)
+	//host := os.Getenv(common.PostgresHost)
+	//user := os.Getenv(common.PostgresUser)
+	//pw := os.Getenv(common.PostgresPassWord)
+	//port := os.Getenv(common.PostgresPort)
+	//dbName := os.Getenv(common.PostgresDB)
+	//ssl := os.Getenv(common.PostgresSSL)
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", host, port, user, dbName, pw, ssl)
-
+	//- POSTGRES_HOST=localhost
+	//- POSTGRES_USER=pg
+	//- POSTGRES_PORT=54321
+	//- POSTGRES_PASSWORD=pass
+	//- POSTGRES_DB=crud
+	//- POSTGRES_SSL=disable
+	//log.Println("host ==>>", host)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s", "localhost", "5433", "pg", "crud", "pass", "disable")
 	client, err = gorm.Open(postgres.Open(dsn))
 	if err != nil {
-		panic("Postgres Client: error:")
+		log.Println("err =>>", err)
 		return nil, err
 	}
 
+	err = MigrateDB(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return client, err
+}
+
+func MigrateDB(dbConn *gorm.DB) (err error) {
+	err = dbConn.AutoMigrate(
+		&model.TbTodoRepositoryModel{},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return err
 }
